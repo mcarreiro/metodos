3ae3b282b17ef8466cd9a4ac9670b68e29e52fd7
Martin Carreiro <martin301290@gmail.com>
2014-08-24 17:43:06 -0300

    asd

--- tp1-mn/src/main.cpp
+++ tp1-mn/src/main.cpp
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <iostream>
 #include <list>
+#include <vector>
 
 using namespace std;
 
-enum status = {VACIO,SANGUIJUELA,FRIO};
+enum statusPoint {VACIO,SANGUIJUELA,FRIO};
 
 struct Point {
     int x;
     int y;
 	float temp;
-	status status;
+	statusPoint status;
 
-	Point(int x, int y, float temp, status status) : x(x), y(y), temp(temp), status(status) {};
+	Point() {
+        temp = NULL;
+        status = VACIO;
+	};
 
+	Point(int xa, int ya, float tempa, statusPoint statusa) : x(xa), y(ya), temp(tempa), status(statusa) {};
 };
 
 class Windshield {
 public:
-    Windshield(){};
+    Windshield(){
+        vector<vector<Point *> > matrix(0, vector<Point *>(0));
+    };
 	Windshield(int x, int y, float ah, int ar);
 	void initialize();
+	void initializeEmptyMatrix();
 
 private:
 	int a;
@@ -30,49 +38,55 @@ private:
 
     int m;
     int n;
-	
-	Punto matrix;
+
+	vector< vector<Point *> > matrix;
 };
 
 Windshield::Windshield(int x, int y, float ah, int ar) {
     a = x;
     b = y;
 	h = ah;
-	r = ar;	
-	
-	initialize();
+	r = ar;
+
+	m = (a/h) + 1;
+	n = (b/h) + 1;
+
+	cout << "ants";
+
+    //1matrix = vector<vector<Point *> >(m, vector<Point *>(n));
+
+    cout << matrix.size();
+
+	this->initialize();
 }
 
 void Windshield::initialize(){
-	m = (a/h) + 1;
-	n = (a/h) + 1;
-
-	initializeEmptyMatrix();
-
-
-
-	
+	this->initializeEmptyMatrix();
 }
 
 void Windshield::initializeEmptyMatrix(){
-	Punto matrix[m][n];
-
+    int i,j;
 	for(i=0; i< m;i++){
 		for (j=0; j<n;j++){
+            matrix[i][j]->y = j;
 			if (j == 0 || i == 0){
-				matrix[i][j] = new Punto()
-			}
-		}		
+//                matrix.at(i).at(j).temp = -100;
+//                matrix.at(i).at(j).status = FRIO;
+            }
+        }
 	}
-
-
 }
 
 int main() {
 	int a,b,h,r;
 	cout << "Ingrese con enters en el medio, a, b, h y r" << "\n";
-	cin >> a >> b >> h >> r;
-    Windshield *Windshield = new Windshield(a,b,h,r);	
+//	cin >> a >> b >> h >> r;
+	a = 2;
+	b = 2;
+	h = 1;
+	r = 1;
+	cout << "ants";
+    Windshield *windshield = new Windshield(a,b,h,r);
 
     return 0;
 }
