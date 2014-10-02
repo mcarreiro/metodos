720d88348472a0c1bbb00d58d498153597403b05
kevinkuja <kevinkuja@gmail.com>
2014-10-02 18:41:36 -0300

    PARA QUE MANOLO JUEGUE

---
+++ tp2-mn/src/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+#include <list>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <stdlib.h>
+#include <fstream>
+#include <algorithm>
+#include <ctime>
+#include "pagerank.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    vector<vector<int> > links = * new vector<vector<int> >();
+    PageRank pr();
+    return 0;
+}
