804d4deab9d39b954a3f8b7679f23a7cf8e48042
Martin Carreiro <martin301290@gmail.com>
2014-10-02 19:52:23 -0300

    indeg

---
+++ tp2-mn/src/indeg.cpp
@@ -0,0 +1,28 @@
+#include "indeg.h"
+#include <utility>      // std::pair
+#include <iostream>     // std::cout
+#include <vector>
+
+//links: en la pos "i" tiene un vector con sus links "j" de salida
+Indeg::Indeg(vector<vector<int> > links){
+
+	int totalAmountOfDomains = links.size();
+
+	values = vector<int>(totalAmountOfDomains);
+
+	for(int i = 0 ; i < values.size() ; i++){
+		values[i] = 0;
+	}
+
+	for(int i = 0; i < links.size(); i++){
+		vector<int> salidas = links[i];
+		int cantSalidas = salidas.size();
+		if(cantSalidas != 0){
+			for(int j = 0; j < cantSalidas; j++){
+				int s = salidas[j]; //Page I goes to S
+				values[s] = values[s] + 1;
+			}
+		}
+	}
+
+}
---
+++ tp2-mn/src/indeg.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Indeg
+{
+public:
+    Indeg(vector<vector<int> >);
+	vector<int> values;
+};
