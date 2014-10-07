533606648e324c4f9d4da1184ea96c8fbb0a151c
kevinkuja <kevinkuja@gmail.com>
2014-10-07 20:01:36 -0300

    TODOSPUTOS

--- tp2-mn/src/main.cpp
+++ tp2-mn/src/main.cpp
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <fstream>
 #include <algorithm>
-#include "dok.cpp"
 #include <ctime>
 #include "pagerank.cpp"
 #include "hits.cpp"
@@ -23,14 +22,18 @@ int main(int argc, char *argv[]) {
     string output = argv[2];
 
     ifstream input_file (input.c_str());
-    string tipoInstancia, path;
-	int alg;
+    string path;
+	int alg, tipoInstancia;	
     double c, tolerancia;
     input_file >> alg >> c >> tipoInstancia >> path >> tolerancia;
     cout  << alg << c << tipoInstancia << path << tolerancia;
 
 
-    vector<vector<int> > links = readFile(path);
+    vector<vector<int> > links;
+    if(tipoInstancia==0)
+		links = leerInstancia0(path);
+	else
+		links = leerInstancia1(path);
 
 
 	if(alg==0) {
@@ -47,6 +50,11 @@ int main(int argc, char *argv[]) {
 					cout << "POS:" << j << " VALOR X: "<< h.x[j] << "\n";
 		for(int j = 0;  j < links.size(); j++)
 					cout << "POS:" << j << " VALOR Y: "<< h.y[j] << "\n";
+	}
+	
+	if(alg==2){
+		Indeg indeg (links);
+		indeg.print();
 	}
 
 
