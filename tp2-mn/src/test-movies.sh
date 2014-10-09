85c29f6b361306278c0fa8c28d403c6cdf509ec1
kevinkuja <kevinkuja@gmail.com>
2014-10-09 18:01:01 -0300

    cosas para testear

---
+++ tp2-mn/src/test-movies.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+g++ main.cpp
+
+#echo Test Movies 0.1
+#./a.out tests/movies-10.in a >> tests/normas/movies-10
+#echo Test Movies 0.2
+#./a.out tests/movies-20.in a >> tests/normas/movies-20
+#echo Test Movies 0.3
+#./a.out tests/movies-30.in a >> tests/normas/movies-30
+#echo Test Movies 0.4
+#./a.out tests/movies-40.in a >> tests/normas/movies-40
+echo Test Movies 0.5
+./a.out tests/movies-50.in a >> tests/normas/movies-50
+echo Test Movies 0.6
+./a.out tests/movies-60.in a >> tests/normas/movies-60
+echo Test Movies 0.7
+./a.out tests/movies-70.in a >> tests/normas/movies-70
+echo Test Movies 0.8
+./a.out tests/movies-80.in a >> tests/normas/movies-80
+echo Test Movies 0.9
+./a.out tests/movies-90.in a >> tests/normas/movies-90
