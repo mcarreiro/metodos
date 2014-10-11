0cdd7e77fbe8a33156163c01b88430a8174a8e5f
Kevin Kujawski <kevinkuja@gmail.com>
2014-10-11 17:54:06 -0300

    INDEG GRABA

--- tp2-mn/src/main.cpp
+++ tp2-mn/src/main.cpp
@@ -37,20 +37,33 @@ int main(int argc, char *argv[]) {
 		links = leerInstancia1(path);
     else 
         links = leerInstancia2(path);
-
+ 	
+ 	ofstream myfile (output);
+	  cout << output;
 
 	if(alg==0) {
         if(tipoInstancia != 2){
 		  PageRank pr(c, tolerancia, links.size(), links);
           pr.ranking(1000);
+
+          		for(int j = 0;  j < links.size(); j++){
+          	myfile << fixed;
+          	myfile << setprecision(5) << pr.v[j] << "\n";
+          }
         }else{
             double cc = atoi(argv[3])/(double)10;
             //cout << "ATOI:" << cc;
           PageRank pr(cc, tolerancia , links.size(), links);
           pr.ranking(1000);
+
+          		for(int j = 0;  j < links.size(); j++){
+          	myfile << fixed;
+          	myfile << setprecision(5) << pr.v[j] << "\n";
+          }
+          
       }
 
-		
+
 			//for(int j = 0;  j < links.size(); j++)
 				//cout << "POS:" << j << " VALOR: "<< pr.v[j] << "\n";
 	}
@@ -58,17 +71,31 @@ int main(int argc, char *argv[]) {
 	if(alg==1){
 		Hits h(links);
 		h.hubsYautoridades(tolerancia);
-		for(int j = 0;  j < links.size(); j++)
-					cout << "POS:" << j << " VALOR X: "<< h.x[j] << "\n";
-		for(int j = 0;  j < links.size(); j++)
-					cout << "POS:" << j << " VALOR Y: "<< h.y[j] << "\n";
+
+		for(int j = 0;  j < links.size(); j++){
+			myfile << fixed;
+			myfile << setprecision(5) << h.x[j] << "\n";
+
+		}
+				
+					
+		for(int j = 0;  j < links.size(); j++){
+			myfile << fixed;
+			myfile << setprecision(5) << h.y[j] << "\n";
+		}
+					
 	}
 	
 	if(alg==2){
 		Indeg indeg (links);
-		indeg.print();
+		//indeg.print();
+		myfile << fixed;
+		 for(int i=0;i<indeg.values.size();i++){
+        myfile << indeg.valuesQuality[i] << endl;
+    }
 	}
 
+	myfile.close();
 
 
 
