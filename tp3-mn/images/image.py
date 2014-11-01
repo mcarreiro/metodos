506821bf24b12c8f16d844b5d7eb29347c3a2d5e
Martin Carreiro <martin301290@gmail.com>
2014-11-01 14:15:12 -0300

    asd

---
+++ tp3-mn/images/image.py
@@ -0,0 +1,30 @@
+import os,sys
+from PIL import Image
+
+print "Ingresar nombre de archivo sin extension:",
+imageName = raw_input()
+imageType = "bmp"
+jpgfile = Image.open(imageName+"."+imageType)
+pix = jpgfile.load()
+height, width = jpgfile.size
+
+for i in xrange(height):
+	for j in xrange(width):
+		acutalTuple = pix[i,j]
+		newTuple = ()
+		if i % 2 == 0: # AZUL-VERDE-AZUL
+			if j % 2 == 0: #AZUL
+				newTuple = (0,0,acutalTuple[2])
+			else: #VERDE
+				newTuple = (0,acutalTuple[1],0)
+		else: # VERDE-ROJO-VERDE
+			if j % 2 == 0: #VERDE
+				newTuple = (0,acutalTuple[1],0)
+			else: #ROJO
+				newTuple = (acutalTuple[0],0,0)
+		pix[i,j] = newTuple
+
+jpgfile.show()
+jpgfile.save(imageName+"_bayer."+imageType,imageType)
+
+print jpgfile.size, jpgfile.format
