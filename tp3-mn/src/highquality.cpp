3623388b12dbe1596f548adf3ddf289f15cff23e
Kevin Kujawski <kevinkuja@gmail.com>
2014-11-03 01:12:10 -0300

    hq

--- tp3-mn/src/highquality.cpp
+++ tp3-mn/src/highquality.cpp
@@ -23,28 +23,27 @@ public:
             for(int j=2; j< height-2; j++) {
                 correccion = 0;
                 if (i %2 == 0 && j % 2 == 0){ //ACA HAY AZUL
-                    
+                  
                     correccion += imagen[i][j-2].azul;
                     correccion += imagen[i-2][j].azul;
                     correccion += imagen[i][j+2].azul;
                     correccion += imagen[i+2][j].azul;
                     correccion = imagen[i][j].azul - correccion/4;
                     imagen[i][j].verde += 0.75 * correccion;
-                    imagen[i][j].rojo += 0.75 * correccion;
+                    //imagen[i][j].rojo += 0.75 * correccion;
                 
                 }else if (i %2 == 1 && j % 2 == 1){ //ACA HAY ROJO
-                    
+                
                     correccion += imagen[i][j-2].rojo;
                     correccion += imagen[i-2][j].rojo;
                     correccion += imagen[i][j+2].rojo;
                     correccion += imagen[i+2][j].rojo;
                     correccion = imagen[i][j].rojo - correccion/4;
                     imagen[i][j].verde += 0.5 * correccion;
-                    imagen[i][j].azul += 0.5 * correccion;
+                    //imagen[i][j].azul += 0.5 * correccion;
 
 
                 } else { //ACA HAY VERDE
-
                     correccion += imagen[i][j-2].verde;
                     correccion += imagen[i-2][j].verde;
                     correccion += imagen[i][j+2].verde;
@@ -56,8 +55,8 @@ public:
                     correccion += imagen[i+1][j-1].verde;
 
                     correccion = imagen[i][j].verde - correccion/8;
-                    imagen[i][j].azul += correccion * 5/8;
-                    imagen[i][j].rojo += correccion * 5/8;
+                    //imagen[i][j].azul += correccion * 5/8;
+                    //imagen[i][j].rojo += correccion * 5/8;
                     
                 } 
                 //cout << "VERDE: " << imagen[i][j].verde << endl;
