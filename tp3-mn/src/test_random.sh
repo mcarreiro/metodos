d5395e5d090abdaa6ceba3130ebf6f20b0ca56bb
Kevin Kujawski <kevinkuja@gmail.com>
2014-11-13 01:41:56 -0300

    test random

---
+++ tp3-mn/src/test_random.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for (( i=100; i<=2500; i=i+100 ))
+do
+	python image_random.py $i $i ../images/test_$i
+	T="$(($(date +'%s')))"
+	python image.py test_$i spline
+	T="$(($(date +'%s')-T))"
+	echo "$i ${T}"
+done
